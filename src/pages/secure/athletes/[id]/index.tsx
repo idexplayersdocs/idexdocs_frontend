cb69dca4b2a4fc9e583328bd1e308cf9165e9043
Hallef Brendo <hallefcoelho@gmail.com>
2024-05-02 14:33:38 -0300

    resolvendo conflitos

--- src/pages/secure/athletes/[id]/index.tsx
+++ src/pages/secure/athletes/[id]/index.tsx
@@ -4,11 +4,28 @@ import SideBar from "@/components/SideBar";
 import { faMugSaucer } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useRouter } from "next/router"
+import { useEffect, useState } from "react";
+import { getAthleteById } from "@/pages/api/athletes";
 
 export default function AthleteDetail() {
   const { query, push, back } = useRouter();
+  const [athlete, setAthlete] = useState<any>();
 
   const athleteId = query?.id;
+
+  
+  useEffect(() => {
+    const fetchAthletesData = async () => {
+      try {
+        const athleteData = await getAthleteById(athleteId);
+        setAthlete(athleteData.data);
+      } catch (error) {
+        console.error('Error fetching athletes:', error);
+      }
+    };
+
+    fetchAthletesData();
+  }, [athleteId]);
 
   return (
     <>
@@ -20,7 +37,7 @@ export default function AthleteDetail() {
         <div className="col-10">
           {/* Relacionamento */}
           <div className="card h-100" style={{backgroundColor: 'var(--bg-secondary-color)', marginRight:'10px'}}>
-              <Relationship />
+              <Relationship athleteData={athlete}/>
           </div>
         </div>
       </div>
