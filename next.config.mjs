d36f0cc100cb4785d941f5858514bc86a5a2b9f1
Hallef Brendo <hallefcoelho@gmail.com>
2024-05-09 16:24:57 -0300

    Resolvendo conlitos

--- next.config.mjs
+++ next.config.mjs
@@ -4,3 +4,10 @@ const nextConfig = {
 };
 
 export default nextConfig;
+
+module.exports = {
+  generateBuildId: async () => {
+    // This could be anything, using the latest git hash
+    return process.env.GIT_HASH
+  },
+}
