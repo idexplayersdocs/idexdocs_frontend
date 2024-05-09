873d4a723ae5ffa890547ec046ca75cf88a00206
Hallef Brendo <hallefcoelho@gmail.com>
2024-05-09 16:26:40 -0300

    resolvendo conflitos

--- next.config.mjs
+++ next.config.mjs
@@ -4,10 +4,3 @@ const nextConfig = {
 };
 
 export default nextConfig;
-
-module.exports = {
-  generateBuildId: async () => {
-    // This could be anything, using the latest git hash
-    return process.env.GIT_HASH
-  },
-}
