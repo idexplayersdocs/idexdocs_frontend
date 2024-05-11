89392220388ba7e3c98793d130dc0e576879f3db
Vittor1108 <vittordaniel1108@gmail.com>
2024-05-11 20:23:59 -0300

    featured: change list to table

--- src/components/AthletePDF.tsx
+++ src/components/AthletePDF.tsx
@@ -54,7 +54,7 @@ export default function AthletePDF({ info, onLoading }: AthletaInfo) {
       ) : (
         <div
           className="bg-white pointer rounded-2 p-4 mx-auto overflow-scroll"
-          style={{ cursor: "pointer", width: "95%", maxHeight: "100%"}}
+          style={{ cursor: "pointer", width: "95%", maxHeight: "100%" }}
           onClick={() => onClickPDF()}
           ref={pdfRef}
         >
@@ -122,29 +122,30 @@ export default function AthletePDF({ info, onLoading }: AthletaInfo) {
                   Características Físicas ({info.atleta.posicao_primaria})
                 </p>
               </div>
-              {info.caracteristicas_fisicas.map((x, i) => {
-                return (
-                  <table className="table">
-                    <thead className="thead-dark">
-                      <tr>
-                        <th className="" scope="col">
-                          Estatura
-                        </th>
-                        <th className="" scope="col">
-                          Envergadura
-                        </th>
-                        <th className="" scope="col">
-                          Peso
-                        </th>
-                        <th className="" scope="col">
-                          Percentual de Gordura
-                        </th>
-                        <th className="" scope="col">
-                          Data
-                        </th>
-                      </tr>
-                    </thead>
-                    <tbody>
+
+              <table className="table">
+                <thead className="thead-dark">
+                  <tr>
+                    <th className="" scope="col">
+                      Estatura
+                    </th>
+                    <th className="" scope="col">
+                      Envergadura
+                    </th>
+                    <th className="" scope="col">
+                      Peso
+                    </th>
+                    <th className="" scope="col">
+                      Percentual de Gordura
+                    </th>
+                    <th className="" scope="col">
+                      Data
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {info.caracteristicas_fisicas.map((x, i) => {
+                    return (
                       <tr>
                         <td className="">{x.estatura}</td>
                         <td className="">{x.envergadura}</td>
@@ -152,10 +153,10 @@ export default function AthletePDF({ info, onLoading }: AthletaInfo) {
                         <td className="">{x.percentual_gordura}</td>
                         <td className="">{x.data_criacao}</td>
                       </tr>
-                    </tbody>
-                  </table>
-                );
-              })}
+                    );
+                  })}
+                </tbody>
+              </table>
             </article>
           </section>
           <section className="mt-4">
@@ -165,59 +166,60 @@ export default function AthletePDF({ info, onLoading }: AthletaInfo) {
                   Características da Posição ({info.atleta.posicao_primaria})
                 </p>
               </div>
-              {info.caracteristicas_posicao.map((x, i) => {
-                return (
-                  <table className="table" style={{maxWidth: "100%", width: "100%"}}>
-                    <thead>
-                      <tr>
-                        <th className="" scope="col" style={{fontSize: 14}}>
-                          Est.
-                        </th>
-                        <th className="" scope="col" style={{fontSize: 14}}>
-                          Veloci.
-                        </th>
-                        <th className="" scope="col" style={{fontSize: 14}}>
-                          1x1
-                        </th>
-                        <th className="" scope="col" style={{fontSize: 14}}>
-                          Desmarq.
-                        </th>
-                        <th className="" scope="col" style={{fontSize: 14}}>
-                          Ctrl de Bola
-                        </th>
-                        <th className="" scope="col" style={{fontSize: 14}}>
-                          Cruzamen.
-                        </th>
-                        <th className="" scope="col" style={{fontSize: 14}}>
-                          Finzali.
-                        </th>
-                        <th className="" scope="col" style={{fontSize: 14}}>
-                          Vis Espacial.
-                        </th>
-                        <th className="" scope="col" style={{fontSize: 14}}>
-                          Dom Orien.
-                        </th>
-                        <th className="" scope="col" style={{fontSize: 14}}>
-                          Dribles
-                        </th>
-                        <th className="" scope="col" style={{fontSize: 14}}>
-                          Leit. De Jogo
-                        </th>
-                        <th className="" scope="col" style={{fontSize: 14}}>
-                          Criativi.
-                        </th>
-                        <th className="" scope="col" style={{fontSize: 14}}>
-                          Capac. De Descição
-                        </th>
-                        <th className="" scope="col" style={{fontSize: 14}}>
-                          Competividade
-                        </th>
-                        <th className="" scope="col" style={{fontSize: 14}}>
-                          Data
-                        </th>
-                      </tr>
-                    </thead>
-                    <tbody>
+
+              <table className="table" style={{ maxWidth: "100%", width: "100%" }}>
+                <thead>
+                  <tr>
+                    <th className="" scope="col" style={{ fontSize: 14 }}>
+                      Est.
+                    </th>
+                    <th className="" scope="col" style={{ fontSize: 14 }}>
+                      Veloci.
+                    </th>
+                    <th className="" scope="col" style={{ fontSize: 14 }}>
+                      1x1
+                    </th>
+                    <th className="" scope="col" style={{ fontSize: 14 }}>
+                      Desmarq.
+                    </th>
+                    <th className="" scope="col" style={{ fontSize: 14 }}>
+                      Ctrl de Bola
+                    </th>
+                    <th className="" scope="col" style={{ fontSize: 14 }}>
+                      Cruzamen.
+                    </th>
+                    <th className="" scope="col" style={{ fontSize: 14 }}>
+                      Finzali.
+                    </th>
+                    <th className="" scope="col" style={{ fontSize: 14 }}>
+                      Vis Espacial.
+                    </th>
+                    <th className="" scope="col" style={{ fontSize: 14 }}>
+                      Dom Orien.
+                    </th>
+                    <th className="" scope="col" style={{ fontSize: 14 }}>
+                      Dribles
+                    </th>
+                    <th className="" scope="col" style={{ fontSize: 14 }}>
+                      Leit. De Jogo
+                    </th>
+                    <th className="" scope="col" style={{ fontSize: 14 }}>
+                      Criativi.
+                    </th>
+                    <th className="" scope="col" style={{ fontSize: 14 }}>
+                      Capac. De Descição
+                    </th>
+                    <th className="" scope="col" style={{ fontSize: 14 }}>
+                      Competividade
+                    </th>
+                    <th className="" scope="col" style={{ fontSize: 14 }}>
+                      Data
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {info.caracteristicas_posicao.map((x, y) => {
+                    return (
                       <tr>
                         <td className="">{x.estatura_fis}</td>
                         <td className="">{x.velocidade_fis}</td>
@@ -235,78 +237,10 @@ export default function AthletePDF({ info, onLoading }: AthletaInfo) {
                         <td className="">{x.competitividade_psi}</td>
                         <td className="">{x.data_criacao}</td>
                       </tr>
-                    </tbody>
-                  </table>
-                  // <div className="row" key={i}>
-                  //   <div className="col-12 d-flex align-items-end flex-wrap mt-5">
-                  //     <p className="fw-bold d-flex align-items-center justify-content-between mb-2 text-uppercase w-100">
-                  //       Estatura:
-                  //       <span className="fw-normal">{x.estatura}</span>
-                  //     </p>
-                  //     <p className="fw-bold d-flex align-items-center justify-content-between mb-2 text-uppercase w-100">
-                  //       Velocidade:
-                  //       <span className="fw-normal">{x.velocidade}</span>
-                  //     </p>
-                  //     <p className="fw-bold d-flex align-items-center justify-content-between mb-2 text-uppercase w-100">
-                  //       Um Contra um ofenviso:
-                  //       <span className="fw-normal">{x.um_contra_um_ofensivo}</span>
-                  //     </p>
-                  //     <p className="fw-bold d-flex align-items-center justify-content-between mb-2 text-uppercase w-100">
-                  //       Desmarques:
-                  //       <span className="fw-normal">{x.desmarques}</span>
-                  //     </p>
-                  //     <p className="fw-bold d-flex align-items-center justify-content-between mb-2 text-uppercase w-100">
-                  //       Controle de Bola:
-                  //       <span className="fw-normal">{x.controle_bola}</span>
-                  //     </p>
-                  //     <p className="fw-bold d-flex align-items-center justify-content-between mb-2 text-uppercase w-100">
-                  //       Cruzamentos:
-                  //       <span className="fw-normal">{x.cruzamentos}</span>
-                  //     </p>
-                  //     <p className="fw-bold d-flex align-items-center justify-content-between mb-2 text-uppercase w-100">
-                  //       Finzaliação:
-                  //       <span className="fw-normal">{x.finalizacao}</span>
-                  //     </p>
-                  //     <p className="fw-bold d-flex align-items-center justify-content-between mb-2 text-uppercase w-100">
-                  //       Visão Espacial:
-                  //       <span className="fw-normal">{x.visao_espacial}</span>
-                  //     </p>
-                  //     <p className="fw-bold d-flex align-items-center justify-content-between mb-2 text-uppercase w-100">
-                  //       Domínio Orientado:
-                  //       <span className="fw-normal">{x.dominio_orientado}</span>
-                  //     </p>
-                  //     <p className="fw-bold d-flex align-items-center justify-content-between mb-2 text-uppercase w-100">
-                  //       Dribles em Diagonal:
-                  //       <span className="fw-normal">{x.dribles_em_diagonal}</span>
-                  //     </p>
-                  //     <p className="fw-bold d-flex align-items-center justify-content-between mb-2 text-uppercase w-100">
-                  //       Leitura de Jogo:
-                  //       <span className="fw-normal">{x.leitura_jogo}</span>
-                  //     </p>
-                  //     <p className="fw-bold d-flex align-items-center justify-content-between mb-2 text-uppercase w-100">
-                  //       Reação pós Perda:
-                  //       <span className="fw-normal">{x.reacao_pos_perda}</span>
-                  //     </p>
-                  //     <p className="fw-bold d-flex align-items-center justify-content-between mb-2 text-uppercase w-100">
-                  //       Criatividade:
-                  //       <span className="fw-normal">{x.criatividade}</span>
-                  //     </p>
-                  //     <p className="fw-bold d-flex align-items-center justify-content-between mb-2 text-uppercase w-100">
-                  //       Capacidade de descição:
-                  //       <span className="fw-normal">{x.capacidade_decisao}</span>
-                  //     </p>
-                  //     <p className="fw-bold d-flex align-items-center justify-content-between mb-2 text-uppercase w-100">
-                  //       Inteligência Tática:
-                  //       <span className="fw-normal">{x.inteligencia_tatica}</span>
-                  //     </p>
-                  //     <p className="fw-bold d-flex align-items-center justify-content-between mb-2 text-uppercase w-100">
-                  //       Competitividade:
-                  //       <span className="fw-normal">{x.competitividade}</span>
-                  //     </p>
-                  //   </div>
-                  // </div>
-                );
-              })}
+                    );
+                  })}
+                </tbody>
+              </table>
             </article>
           </section>
           <section className="mt-4">
@@ -314,19 +248,25 @@ export default function AthletePDF({ info, onLoading }: AthletaInfo) {
               <div className="border-bottom border-4 border-black mb-3">
                 <p className="fw-bold mb-2 text-uppercase h4">Histórico de Lesões ({info.atleta.posicao_primaria})</p>
               </div>
-              {info.lesao.map((x, i) => {
-                return (
-                  <div className="row" key={i}>
-                    <p className="fw-bold d-flex align-items-center justify-content-between mb-2 text-uppercase w-100">
-                      Descrição:
-                      <span className="fw-normal">
-                        {" "}
-                        {x.data_lesao} - {x.descricao}
-                      </span>
-                    </p>
-                  </div>
-                );
-              })}
+
+              <table className="table" style={{ maxWidth: "100%", width: "100%" }}>
+                <thead>
+                  <tr>
+                    <th>Descrição</th>
+                    <th>Data</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {info.lesao.map((x, i) => {
+                    return (
+                      <tr>
+                        <td>{x.descricao}</td>
+                        <td>{x.data_lesao}</td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
             </article>
           </section>
           <section className="mt-4">
@@ -334,32 +274,60 @@ export default function AthletePDF({ info, onLoading }: AthletaInfo) {
               <div className="border-bottom border-4 border-black mb-3">
                 <p className="fw-bold mb-2 text-uppercase h4">Histórico de Clubes ({info.atleta.posicao_primaria})</p>
               </div>
-              {info.clube.map((x, i) => {
-                return (
-                  <div className="row border-bottom border-2 mb-2 border-gray" key={i}>
-                    <p className="fw-bold d-flex align-items-center justify-content-between mb-2 text-uppercase w-100">
-                      Nome:
-                      <span className="fw-normal"> {x.nome}</span>
-                    </p>
-                    <p className="fw-bold d-flex align-items-center justify-content-between mb-2 text-uppercase w-100">
-                      Data de Inicio:
-                      <span className="fw-normal"> {x.data_inicio}</span>
-                    </p>
-                    <p className="fw-bold d-flex align-items-center justify-content-between mb-2 text-uppercase w-100">
-                      Data de Fim:
-                      <span className="fw-normal"> {x.data_fim}</span>
-                    </p>
-                  </div>
-                );
-              })}
+              <table className="table">
+                <thead>
+                  <tr>
+                    <td>Nome</td>
+                    <td>Date de Inicio</td>
+                    <td>Data de Fim</td>
+                  </tr>
+                </thead>
+                <tbody>
+                  {info.clube.map((x, i) => {
+                    return (
+                      <tr>
+                        <td>{x.nome}</td>
+                        <td>{x.data_inicio}</td>
+                        <td>{x.data_fim}</td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
             </article>
           </section>
           <section className="mt-4">
             <article>
               <div className="border-bottom border-4 border-black mb-3">
-                <p className="fw-bold mb-2 text-uppercase h4">Histórico de competição ({info.atleta.posicao_primaria})</p>
+                <p className="fw-bold mb-2 text-uppercase h4">
+                  Histórico de competição ({info.atleta.posicao_primaria})
+                </p>
               </div>
-              {info.clube.map((x, i) => {
+              <table className="table">
+                <thead>
+                  <tr>
+                    <td>Nome</td>
+                    <td>Gols</td>
+                    <td>Jogos Completos</td>
+                    <td>Jogos Parciais</td>
+                    <td>Minutagem</td>
+                  </tr>
+                </thead>
+                <tbody>
+                  {info.competicao.map((x, i) => {
+                    return (
+                      <tr>
+                        <td>{x.nome}</td>
+                        <td>{x.gols}</td>
+                        <td>{x.jogos_completos}</td>
+                        <td>{x.jogos_parciais}</td>
+                        <td>{x.minutagem}</td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+              {/* {info.clube.map((x, i) => {
                 return (
                   <div className="row border-bottom border-2 mb-2 border-gray" key={i}>
                     <p className="fw-bold d-flex align-items-center justify-content-between mb-2 text-uppercase w-100">
@@ -376,7 +344,7 @@ export default function AthletePDF({ info, onLoading }: AthletaInfo) {
                     </p>
                   </div>
                 );
-              })}
+              })} */}
             </article>
           </section>
           <section className="mt-5">
