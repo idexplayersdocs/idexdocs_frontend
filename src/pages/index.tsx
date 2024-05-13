86ef1c3eb05d07da6e12d32cfec945d3546dedb8
Hallef Brendo <hallefcoelho@gmail.com>
2024-05-13 09:55:53 -0300

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Athletes from "./secure/athletes"
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
-import './globals.css';
+import "@/styles/globals.css";
 
 export default function Index() {
   const router = useRouter();
