0c099fe475f3b252b6904c656d4f03143c0c8e39
Hallef Brendo <hallefcoelho@gmail.com>
2024-05-13 11:13:48 -0300

    fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,8 @@
-// import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { useRouter } from "next/router";
 import React from "react";
-
-import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -17,11 +15,5 @@ export default function App({ Component, pageProps }: AppProps) {
     }
   }, [router.pathname]);
 
-  return <>
-   <Head> 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossOrigin="anonymous" /> 
-    </Head>
-  <Component {...pageProps} />;
-  </>
-  
+  return <Component {...pageProps} />;
 }
