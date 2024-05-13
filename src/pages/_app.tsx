4a10316c76c3ad283533eab0a9bc93790cda12dc
Hallef Brendo <hallefcoelho@gmail.com>
2024-05-13 10:28:20 -0300

    fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import "@/styles/css/style.css";
+import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { useRouter } from "next/router";
 import React from "react";
