df7199c29af49969d5a65eb61cd1f6c5bbe06866
Hallef Brendo <hallefcoelho@gmail.com>
2024-05-12 23:36:05 -0300

    fix

--- src/components/modal/PerformanceCreation.tsx
+++ src/components/modal/PerformanceCreation.tsx
@@ -42,7 +42,7 @@ export default function PerformanceCreation({closeModal, athleteId, dataList, la
   // Fisico
 const reorderedKeysLabelFisico = labelList.fisico.filter((item:any) => item !== "Total" && item !== "Media");
 
-console.log(reorderedKeysLabelFisico);
+// console.log(reorderedKeysLabelFisico);
 
 
 
@@ -65,8 +65,6 @@ const reorderedKeysDataFisico: { [key: string]: any } = {
       .reduce((acc, [key, value]) => ({ ...acc, [key]: value }), {})
 };
 const [formDataListFisico, setFormDataListFisico] = useState(reorderedKeysDataFisico)
-
-  console.log(formDataListFisico['controle_bola_fis'])
   // useEffect(() => {
   //   if (!effectRan.current) {
   //     setFormDataList(dataList);
@@ -74,14 +72,46 @@ const [formDataListFisico, setFormDataListFisico] = useState(reorderedKeysDataFi
   //   }
   // }, [dataList]);
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>, index: number) => {
-    const { name, value } = e.target;
-  
-    // const newData = { ...dataList };
-    // newData.fisico[index][name] = value;
-    // setFormDataList(newData);
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormDataListFisico((prevState: any) => ({
+      ...prevState,
+      [name]: value,
+    }));
     console.log(formDataListFisico)
   };
+
+
+
+  const handleSaverRegister = async () => {
+    setLoading(true);
+    try {
+      console.log(formDataListFisico)
+
+      // const response = await createPhysical(formRegisterPhysical);
+      // handleCloseRegisterPhysical();
+      // setPage(1)
+      // const clubList = await getPhysical(athleteId, page, 'fisico');
+      // setPhysical(clubList?.data);
+      // setTotalRow(clubList?.total);
+    } catch (error:any) {
+      toast.error(error.response.data.errors[0].message, {
+        position: "bottom-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+        transition: Bounce,
+        });
+    } finally {
+      setLoading(false);
+    }
+  };
+
+
+
 
   if (loading) {
     return (
@@ -93,31 +123,35 @@ const [formDataListFisico, setFormDataListFisico] = useState(reorderedKeysDataFi
 
   return (
     <>
-  <div className='d-flex justify-content-between'>
-    <Subtitle subtitle='Criar Caracteristicas'/>
-    <FontAwesomeIcon icon={faX} color='white' size='xl' style={{cursor: 'pointer'}} onClick={handleCloseModal}/>
-  </div>
-  <hr />
-  <div className='row' style={{maxHeight: '500px', overflow: 'auto'}}>
-    <div className='col' style={{borderRight: '1px solid white'}}>
-      <h3>PERFIL FÍSICO E TÉCNICO</h3>
-      {reorderedKeysLabelFisico.map((label: any, index: number) => (
-        <div key={index} className="d-flex flex-column w-100 mt-3">
-          <label className="ms-3" style={{color: 'white', fontSize: '20px'}}>{label}</label>
-          <input 
-            type="number" 
-            className="form-control input-create input-date bg-dark" 
-            placeholder={label} 
-            name={label} 
-            style={{height:'45px'}} 
-            value={formDataListFisico[formDataListFisico[label]]} // Usando o labelMapping para acessar o valor correto em formDataListFisico
-            onChange={(e) => handleChange(e, index)} 
-          />
+      <div className='d-flex justify-content-between'>
+        <Subtitle subtitle='Criar Caracteristicas'/>
+        <FontAwesomeIcon icon={faX} color='white' size='xl' style={{cursor: 'pointer'}} onClick={handleCloseModal}/>
+      </div>
+      <hr />
+      <div className='row' style={{maxHeight: '500px', overflow: 'auto'}}>
+      <div className='col' style={{borderRight: '1px solid white'}}>
+        <h3>PERFIL FÍSICO E TÉCNICO</h3>
+        {reorderedKeysLabelFisico.map((label: any, index: number) => (
+          <div key={index} className="d-flex flex-column w-100 mt-3">
+            <label className="ms-3" style={{color: 'white', fontSize: '20px'}}>{label}</label>
+            <input 
+              type={Object.keys(formDataListFisico)[index] === 'data_avaliacao' ? 'date' : 'number'} 
+              className="form-control input-create input-date bg-dark" 
+              placeholder={label} 
+              name={Object.keys(formDataListFisico)[index]}
+              style={{height:'45px'}} 
+              value={formDataListFisico[formDataListFisico[index]]} // Usando o labelMapping para acessar o valor correto em formDataListFisico
+              // value={formDataListFisico[Object.keys(formDataListFisico)[0]]} 
+              onChange={handleChange}
+            />
+          </div>
+        ))}
+      </div>
+      <div className='ms-3 d-flex flex-column' style={{width: '98%'}}>
+        <button type="button" className="btn btn-success align-self-end" style={{width:'auto'}} onClick={handleSaverRegister}>Salvar</button>
         </div>
-      ))}
-    </div>
-  </div>
-  <ToastContainer />
+      </div>
+      <ToastContainer />
     </>
   );
   
