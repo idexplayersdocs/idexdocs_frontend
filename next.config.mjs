3e5b334a3000942b1cbc556759ce42b4119a0327
Igor de Freitas Cruz <igor.freitas.cruz@icloud.com>
2024-06-07 00:30:01 -0300

    config: atualização das configurações de imagem no arquivo next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
+
+if (!process.env.API_URL) {
+  throw new Error('The API_URL environment variable is not defined!');
+}
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['idexdocsblob.blob.core.windows.net'],
+    remotePatterns: [{
+      protocol: 'https',
+      hostname: 'idexdocsblob.blob.core.windows.net'
+    },],
   },
   env: {
     API_URL: process.env.API_URL
